0ae3d8ee2dabcf24995184710d19f2114eb80267
hsuehly <207195095@qq.com>
2021-08-13 21:48:20 +0800

    计算属性

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,6 +3,8 @@
 <p>{{helloRef}}</p>
 <router-link to="/cou">去计算页面</router-link>
 <router-link to="/per">去展示页面</router-link>
+<p>{{num}}</p>
+<button @click="getname">修改num</button>
 <Demo @saihai="saihai">
   <template #data >
     <p>我是内容</p>
@@ -11,7 +13,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import Demo from './solt.vue'
 export default defineComponent({
   components: {Demo},
@@ -26,11 +28,30 @@ export default defineComponent({
     const saihai = (value: string) => {
       alert(value +"你好")
     }
+    //普通写法
+    const num2 = computed(() => helloRef.value + " sss")
+    // 完全写法
+    const num = computed({
+      get() {
+        return helloRef.value + "有人调用"
+      },
+      set(value: string) {
+        console.log("+++++",value);
+        
+
+      }
+    })
+
+    const getname = () => {
+      num.value = " hahaha"
+    }
 
     return {
       helloRef,
       sayhai,
       saihai,
+      num,
+      getname
     }
   }
 })
