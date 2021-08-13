92b2d71fb779cf1749ba90c6d132fddf1b7cb57b
hsuehly <207195095@qq.com>
2021-08-13 23:19:36 +0800

    组合式生命周期

---
+++ src/组合式api生命周期钩子/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>我是父组件</p>
+  <button @click="showRef = !showRef">卸载子组件</button>
+  <hr />
+  <Demo  v-if="showRef"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Demo from './test.vue'
+export default defineComponent({
+  components: {Demo},
+  setup() {
+    const showRef = ref(true)
+
+
+    
+    return {
+      showRef
+    }
+  },
+})
+</script>
---
+++ src/组合式api生命周期钩子/test.vue
@@ -0,0 +1,80 @@
+<template>
+  <p>我是子组件</p>
+  <p>数字: {{numRef}}</p>
+  <button @click="numRef ++">+1</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    // 组合api 的生命周期钩子
+    const numRef = ref(0)
+
+    onBeforeMount(()=> {
+      console.log("---onBeforeMount---");
+      
+    })
+    onMounted(()=> {
+      console.log("---onMounted---");
+
+    })
+    onBeforeUpdate(()=> {
+      console.log("---onBeforeUpdate---");
+
+    })
+    onUpdated(()=> {
+      console.log("---onUpdated---");
+
+    }),
+    onBeforeUnmount(()=> {
+      console.log("---onBeforeUnmount---");
+
+    })
+    onUnmounted(()=> {
+      console.log("---onUnmounted---");
+
+    })
+    
+
+    return {
+      numRef
+    
+    }
+  },
+  //#region 
+//   beforeCreate() {
+//     console.log("----beforeCreate----")
+//   },
+//   created() {
+//     console.log("----created----")
+
+//   },
+//   beforeMount() {
+//     console.log("----beforeMount----")
+
+//   },
+//   mounted() {
+//     console.log("----mounted----")
+
+//   },
+//   beforeUpdate() {
+//     console.log("----beforeUpdate----")
+
+//   },
+//   updated() {
+//     console.log("----updated----")
+
+//   },
+//  beforeUnmount() {
+//   console.log("----beforeUnmount----")
+
+//  },
+//  unmounted() {
+//   console.log("----unmounted----")
+
+//  }
+//#endregion
+})
+</script>
